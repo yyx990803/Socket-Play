b61252a98b8ccba0544eea5240f89445d73f485b
Evan You <yyx990803@gmail.com>
2011-11-02 06:11:46 -0400

    123

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.configure('production', function(){
 app.get('/', controllers.index);
 app.get('/c', controllers.controller);
 
-app.listen(8888);
+app.listen(1234);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
