8bbeb8ab32d3ee420574f6651bbf15ba878dee0b
Evan You <yyx990803@gmail.com>
2011-11-02 07:52:18 -0400

    123

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.listen(8888);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
+  socket.on('update', function (data) {
     console.log(data);
   });
 });
